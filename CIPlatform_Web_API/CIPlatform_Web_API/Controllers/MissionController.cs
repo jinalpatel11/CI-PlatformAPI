934d77a7a1df5b2f554ad6c3cf47710a2544f034
jinalpatel11 <Jpatel6625@conestogac.on.ca>
2023-10-04 09:09:35 +0530

    create mission controller

---
+++ CIPlatform_Web_API/CIPlatform_Web_API/Controllers/MissionController.cs
@@ -0,0 +1,185 @@
+﻿using CIPlatform_Web_API.Infrastructure;
+using CIPlatform_Web_API.Models;
+using CIPlatform_Web_API.Models.Request;
+using CIPlatform_Web_API.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CIPlatform_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MissionController : Controller
+    {
+        private readonly IMissionTableRepository missionTableRepository;
+        protected APIResponse _response;
+        public MissionController(IMissionTableRepository missionTableRepository)
+        {
+            this.missionTableRepository = missionTableRepository;
+            _response = new();
+        }
+
+
+        [HttpGet("{id:int}", Name = "GeMission")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> GeMission(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+                var villa = await missionTableRepository.GetMissionTableById(id);
+                if (villa == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+                _response.Result = villa;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetMissions()
+        {
+            try
+            {
+                IEnumerable<MissionTable> missionTables;
+                missionTables = await missionTableRepository.GetMissionTable();
+                _response.Result = missionTables;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreatetMission([FromBody] MissionRequestModel createDTO)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+
+                if (createDTO == null)
+                {
+                    return BadRequest(createDTO);
+                }
+
+                MissionTable mission = createDTO.ToEntity();
+
+                mission = await missionTableRepository.AddMissionTable(mission);
+                _response.Result = mission;
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtRoute("GetMission", new { id = mission.Id }, _response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpDelete("{id:int}", Name = "DeletetMission")]
+        public async Task<ActionResult<APIResponse>> DeletetMission(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    return BadRequest();
+                }
+                var villa = await missionTableRepository.GetMissionTableById(id);
+                if (villa == null)
+                {
+                    return NotFound();
+                }
+                await missionTableRepository.DeleteMissionTable(villa);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateMission")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<APIResponse>> UpdateMission(int id, [FromBody] MissionRequestModel updateDTO)
+        {
+            try
+            {
+                if (updateDTO == null)
+                {
+                    return BadRequest();
+                }
+
+                MissionTable dbMission = await this.missionTableRepository.GetMissionTableById(id);
+                MissionTable model = updateDTO.ToEntity();
+
+                await missionTableRepository.UpdateMissionTable(dbMission, model);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+    }
+
+}
+
---
+++ CIPlatform_Web_API/CIPlatform_Web_API/Models/Request/MissionRequestModel.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using CIPlatform_Web_API.Infrastructure;
+
+namespace CIPlatform_Web_API.Models.Request
+{
+    public class MissionRequestModel
+    {
+        public int? CountryId { get; set; }
+
+        public int? CityId { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionTitle { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionDescription { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionOrganisationName { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionOrganisationDetail { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? MissionStartDate { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? MissionEndDate { get; set; }
+
+        public int? TotalSeats { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? MissionRegistrationDeadline { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionTheme { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionSkills { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionImages { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionDocumnets { get; set; }
+
+        [StringLength(10)]
+        [Unicode(false)]
+        public string? MissionAvailability { get; set; }
+
+
+        public MissionTable ToEntity()
+        {
+            return new MissionTable
+            {
+               CityId = this.CityId,
+               CountryId= this.CountryId,
+               MissionAvailability  = this.MissionAvailability,
+               MissionDescription = this.MissionDescription ,
+               MissionDocumnets = this.MissionDocumnets   ,
+               MissionEndDate = this.MissionEndDate     ,
+               MissionImages = this.MissionImages   ,
+               MissionOrganisationDetail = this.MissionOrganisationDetail   ,
+               MissionOrganisationName= this.MissionOrganisationName ,
+               MissionRegistrationDeadline = this.MissionRegistrationDeadline ,
+               MissionSkills= this.MissionSkills ,
+               MissionStartDate= this.MissionStartDate ,
+               MissionTheme= this.MissionTheme ,
+               MissionTitle = this.MissionTitle ,   
+               TotalSeats= this.TotalSeats ,
+            };
+        }
+    }
+}
